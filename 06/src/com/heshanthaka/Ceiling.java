15926da48ba5ce51d85e8ef394a0aebc6a1c9127
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-11 16:06:27 +0530

    Implementation of composition by creating a bedroom test class

---
+++ 06/src/com/heshanthaka/Ceiling.java
@@ -0,0 +1,19 @@
+package com.heshanthaka;
+
+public class Ceiling {
+    private int height;
+    private String color;
+
+    public Ceiling(int height, String color) {
+        this.height = height;
+        this.color = color;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
