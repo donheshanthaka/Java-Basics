02928570567d9f6be6b10c2612c88e0df71e212a
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-11 15:37:44 +0530

    Implementation of a car class with inheritance

--- 05/src/com/heshanthaka/Main.java
+++ 05/src/com/heshanthaka/Main.java
@@ -2,4 +2,11 @@ package com.heshanthaka;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Prius prius01 = new Prius(100);
+        System.out.println(prius01.getName());
+        prius01.move(2,3);
+        System.out.println(prius01.getCurrentVelocity());
+    }
 }
