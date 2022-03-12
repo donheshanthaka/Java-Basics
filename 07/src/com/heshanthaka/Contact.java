633f72e3f381d8be2b39207ef99e3f5a79d131f0
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-12 21:46:17 +0530

    MobilePhone:- Creation of the main and contact class

---
+++ 07/src/com/heshanthaka/Contact.java
@@ -0,0 +1,23 @@
+package com.heshanthaka;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber){
+        return new Contact(name, phoneNumber);
+    }
+}
